26e85bdd0238704f4a949655e8e6b713a20a8261
Victor Li <victorli5674@yahoo.com>
2017-12-10 20:13:23 -0800

    removed unused libraries for backend, and fixed up code for back-end

--- server/index.js
+++ server/index.js
@@ -1,16 +1,9 @@
 import express from 'express';
-import cookieParser from 'cookie-parser';
-import bodyParser from 'body-parser';
 import path from 'path';
 
-import dbConnection from './models/dbConnection';
 import libraries from './routes/libraries';
 
 const app = express();
-
-app.use(cookieParser());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static(path.join(__dirname, '../client/build')));
 
@@ -22,11 +15,6 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
-dbConnection.then((db) => {
-  // set db instance to postgres database
-  app.set('db', db);
-
-  app.listen(process.env.PORT, () => {
-    console.log(`Listening on port ${process.env.PORT}`);
-  });
+app.listen(process.env.PORT, () => {
+  console.log(`Listening on port ${process.env.PORT}`);
 });
