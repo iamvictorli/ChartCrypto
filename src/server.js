e0addddfaf0f83f5403fd0361d0f02f94d0e09d6
Victor Li <victorli5674@yahoo.com>
2018-02-14 14:10:19 -0800

    made redis requests in seperate file

--- src/server.js
+++ src/server.js
@@ -2,8 +2,8 @@ import express from 'express';
 import http from 'http';
 import socketIO from 'socket.io';
 import next from 'next';
-import redis from 'redis';
-import { promisify } from 'util';
+
+import { addStock, deleteStock, getAllStocks } from '../utils/redis';
 
 const app = express();
 const server = http.Server(app);
@@ -14,44 +14,22 @@ const dev = process.env.NODE_ENV !== 'production';
 const nextApp = next({ dev });
 const nextHandler = nextApp.getRequestHandler();
 
-const client = redis.createClient();
-client.on('connect', () => console.log('Connected to Redis'));
-const zAddAsync = promisify(client.zadd).bind(client);
-const zRangeAync = promisify(client.zrange).bind(client);
-const zRemRangeByScoreAsync = promisify(client.zremrangebyscore).bind(client);
-
 io.on('connection', socket => {
   console.log('a user connected');
 
   // send to clients except sender about someone's newly added stock
   socket.on('Add Stock', stock => {
-    zAddAsync('Stocks', stock.id, stock.value).then(() => {
-      zRangeAync('Stocks', 0, -1, 'WITHSCORES').then(stocksWithScores => {
-        const s = [];
-        for (let i = 0; i < stocksWithScores.length; i += 2) {
-          s.push({
-            id: Number(stocksWithScores[i + 1]),
-            value: stocksWithScores[i]
-          });
-        }
-        console.log(s);
-        io.emit('Add Stock', s);
+    addStock(stock.id, stock.value).then(() => {
+      getAllStocks().then(stocks => {
+        io.emit('Add Stock', stocks);
       });
     });
   });
 
   socket.on('Delete Stock', stockID => {
-    zRemRangeByScoreAsync('Stocks', stockID, stockID).then(() => {
-      zRangeAync('Stocks', 0, -1, 'WITHSCORES').then(stocksWithScores => {
-        const s = [];
-        for (let i = 0; i < stocksWithScores.length; i += 2) {
-          s.push({
-            id: Number(stocksWithScores[i + 1]),
-            value: stocksWithScores[i]
-          });
-        }
-        console.log(s);
-        io.emit('Delete Stock', s);
+    deleteStock(stockID).then(() => {
+      getAllStocks().then(stocks => {
+        io.emit('Delete Stock', stocks);
       });
     });
   });
@@ -63,22 +41,7 @@ io.on('connection', socket => {
 
 nextApp.prepare().then(() => {
   app.get('/stocks', (req, res) => {
-    // lRangeAsync('Stocks', 0, -1).then(stocks => {
-    //   stocks.forEach(stock => console.log(stock));
-    //   res.json(stocks);
-    // });
-    zRangeAync('Stocks', 0, -1, 'WITHSCORES').then(stocksWithScores => {
-      const stocks = [];
-      for (let i = 0; i < stocksWithScores.length; i += 2) {
-        stocks.push({
-          id: Number(stocksWithScores[i + 1]),
-          value: stocksWithScores[i]
-        });
-      }
-      console.log(stocks);
-
-      res.json(stocks);
-    });
+    getAllStocks().then(stocks => res.json(stocks));
   });
 
   app.get('*', (req, res) => nextHandler(req, res));
---
+++ utils/redis.js
@@ -0,0 +1,22 @@
+import redis from 'redis';
+import { promisify } from 'util';
+
+const client = redis.createClient();
+client.on('connect', () => console.log('Connected to Redis'));
+const zAddAsync = promisify(client.zadd).bind(client);
+const zRangeAync = promisify(client.zrange).bind(client);
+const zRemRangeByScoreAsync = promisify(client.zremrangebyscore).bind(client);
+
+export const addStock = (id, value) => zAddAsync('Stocks', id, value);
+export const deleteStock = stockID => zRemRangeByScoreAsync('Stocks', stockID, stockID);
+export const getAllStocks = () =>
+  zRangeAync('Stocks', 0, -1, 'WITHSCORES').then(stocksWithScores => {
+    const stocks = [];
+    for (let i = 0; i < stocksWithScores.length; i += 2) {
+      stocks.push({
+        id: Number(stocksWithScores[i + 1]),
+        value: stocksWithScores[i]
+      });
+    }
+    return stocks;
+  });
