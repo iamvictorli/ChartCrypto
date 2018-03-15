7486c481f32b77e88bff9359062481584aaf12bc
Victor Li <victorli5674@yahoo.com>
2018-03-15 11:56:49 -0700

    using bluebird to promisify redis

--- utils/redis.js
+++ utils/redis.js
@@ -1,25 +1,22 @@
 // @flow
 
 import redis from 'redis';
-import { promisify } from 'util';
+import bluebird from 'bluebird';
+
+bluebird.promisifyAll(redis.RedisClient.prototype);
+bluebird.promisifyAll(redis.Multi.prototype);
 
 const client = redis.createClient(process.env.REDIS_URL || 'redis://localhost:6379');
 client.on('connect', () => console.log('Connected to Redis'));
-const hSetAsync = promisify(client.hset).bind(client);
-const hGetAllAsync = promisify(client.hgetall).bind(client);
-const hDelAsync = promisify(client.hdel).bind(client);
-
-const sAddAsync = promisify(client.sadd).bind(client);
-const sMembersAsync = promisify(client.smembers).bind(client);
 
 export const setUserList = (currencyTitle: string, currencyMetaData: string) =>
-  hSetAsync('userList', currencyTitle, currencyMetaData);
+  client.hsetAsync('userList', currencyTitle, currencyMetaData);
 
 export const getUserList = async () => {
-  // hGetAll returns a huge object with key and values
+  // returns a huge object with key and values
   // need to make array of objects
   const list = [];
-  const currencyObject = await hGetAllAsync('userList');
+  const currencyObject = await client.hgetallAsync('userList');
   if (currencyObject === null) return list;
   Object.entries(currencyObject).forEach(([title, metaData]) => {
     list.push({ title, metaData });
@@ -28,8 +25,8 @@ export const getUserList = async () => {
 };
 
 export const deleteUserList = (currencyTitle: string) => {
-  hDelAsync('userList', currencyTitle);
+  client.hdelAsync('userList', currencyTitle);
 };
 
-export const populateCurrencyList = (currency: string) => sAddAsync('currencyList', currency);
-export const getCurrencyList = () => sMembersAsync('currencyList');
+export const populateCurrencyList = (currency: string) => client.saddAsync('currencyList', currency);
+export const getCurrencyList = () => client.smembersAsync('currencyList');
